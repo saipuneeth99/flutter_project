848521977c401db96ddc0b4d17640cc0a1ef63e3
saipuneeth99 <yerramsetti.12322424@lpu.in>
2025-11-21 23:20:20 +0530

    test: match demo app (Git Repo / Hello, Git!)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_adv/main.dart';
 
 void main() {
-  testWidgets('Demo app displays title and body', (WidgetTester tester) async {
-    // Build the current app and trigger a frame.
+  testWidgets('Demo app shows title and body', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
 
     // Verify AppBar title is 'Git Repo'.
     expect(find.text('Git Repo'), findsOneWidget);
